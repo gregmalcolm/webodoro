725da1410a1dc1653a3defae7b809912ee2df254
gregmalcolm <greg@gregmalcolm.com>
2011-06-25 15:44:36 -0400

    Fresh rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webodoro::Application.config.secret_token = '2b92cde7ff19df5c6beec3c5541b2cdf753411ec80989d18fa0d8524e281fec9d2a163b1ef75fe4c92fb4d9ff8edbde73a6052230fe724ebb1f1b6edc7614d79'
